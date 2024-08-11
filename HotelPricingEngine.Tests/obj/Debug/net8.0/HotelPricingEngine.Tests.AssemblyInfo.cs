029dad5fb352254939c689337e5767fd12695b0e
YassineJlassi123 <yassinej696@gmail.com>
2024-08-11 15:56:55 +0200

    Update HotelPricingEngine.Tests.AssemblyInfo.cs

--- HotelPricingEngine.Tests/obj/Debug/net8.0/HotelPricingEngine.Tests.AssemblyInfo.cs
+++ HotelPricingEngine.Tests/obj/Debug/net8.0/HotelPricingEngine.Tests.AssemblyInfo.cs
@@ -10,13 +10,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HotelPricingEngine.Tests")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("HotelPricingEngine.Tests")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HotelPricingEngine.Tests")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
